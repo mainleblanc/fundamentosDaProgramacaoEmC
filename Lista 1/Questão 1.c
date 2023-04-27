f9837d8d4520535c2ef32c680a8db3a2d7c174d3
mainleblanc <pedro_henrique_2017_@outlook.com>
2023-04-27 01:31:06 -0300

    locale.h adicionada

--- Lista 1/Questão 1.c
+++ Lista 1/Questão 1.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include <locale.h>
 
 /*1. Faça um programa que leia um número inteiro e o imprima.*/
 
 int main(){
 
+    setlocale(LC_ALL, "Portuguese");
     int num;
 
         printf("Insira um inteiro: ");
